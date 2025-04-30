b922caf0db7a713c2c04e910d682a111f3714fff
maysahabbash92 <maysahabbash92@gmail.com>
2025-04-30 23:57:48 +0300

    fixed error

--- backend/urls.py
+++
@@ -1,8 +0,0 @@
-#connect urls.py//base to django project
-from django.contrib import admin
-from django.urls import path, include 
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('base.urls')) #includes in project
-]
