7a858baf306d0b8d9768ee217a72633e5dea2e29
maysahabbash92 <maysahabbash92@gmail.com>
2025-05-02 02:36:31 +0300

    Add customized tokens (refresh) (access)

--- backend/base/views.py
+++ backend/base/views.py
@@ -3,6 +3,41 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .models import MyUser
 from .serializers import MyUserProfileSerializer
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        tokens = response.data
+
+        access_token = tokens ['access']
+        refresh_token = tokens['refresh']
+
+        res = Response()
+        res = {"success":True}
+        res.set_cookie(
+           key = 'access_token',
+           value=access_token,
+           httponly = True,
+           secure = True,
+           samesite='None',
+           path='/'
+
+        )
+        res.set_cookie(
+           key = 'refresh_token',
+           value=refresh_token,
+           httponly = True,
+           secure = True,
+           samesite='None',
+           path='/'
+
+        )
+        return res
+
 
 
 # Create your views here.
