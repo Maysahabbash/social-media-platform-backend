93e57e47252b03c2b17f319d3b9e52d0c4302cb1
maysahabbash92 <maysahabbash92@gmail.com>
2025-05-03 02:13:27 +0300

    Add User Register Serializer

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -1,6 +1,26 @@
 from rest_framework import serializers
 from .models import MyUser
 
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    class meta:
+        model = MyUser
+        fields = ['username','email','first_name', 'last_name', 'password']
+    
+    def create(self, validated_data):
+        user = MyUser(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+            
+        )
+
+        user.set_passwor(validated_data['password'])
+        user.save()
+        return user
+
+        return super().create(validated_data)
 
 class MyUserProfileSerializer(serializers.ModelSerializer):
     followers_count = serializers.SerializerMethodField()
